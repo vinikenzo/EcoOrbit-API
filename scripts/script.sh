724c768ab955b26eecb6629d04e6961f479c1d53
João Victor <joaovictoralves087@gmail.com>
2026-06-09 12:50:40 -0300

    Adição do script

---
+++ scripts/script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+RG="rg-EcoOrbit"
+LOCATION="brazilsouth"
+ACR_NAME="acrecorbit$RANDOM"
+PLAN_NAME="plan-ecoorbit"
+
+az group create \
+  --name $RG \
+  --location $LOCATION
+
+az acr create \
+  --resource-group $RG \
+  --name $ACR_NAME \
+  --sku Basic \
+  --admin-enabled true
+
+az appservice plan create \
+  --name $PLAN_NAME \
+  --resource-group $RG \
+  --is-linux \
+  --sku B1
+
+az webapp create \
+  --resource-group $RG \
+  --plan $PLAN_NAME \
+  --name ecoorbit-api-server \
+  --deployment-container-image-name nginx:latest
+
+az webapp create \
+  --resource-group $RG \
+  --plan $PLAN_NAME \
+  --name ecoorbit-api-usuario \
+  --deployment-container-image-name nginx:latest
+
+az webapp create \
+  --resource-group $RG \
+  --plan $PLAN_NAME \
+  --name ecoorbit-api-ia \
+  --deployment-container-image-name nginx:latest
+
+az webapp create \
+  --resource-group $RG \
+  --plan $PLAN_NAME \
+  --name ecoorbit-api-predict \
+  --deployment-container-image-name nginx:latest
